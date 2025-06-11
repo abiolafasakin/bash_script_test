12f1a8b7be679c06d419f4e0c312a4cdbca820d8
Abiola <abiolafasakin10@gmail.com>
2025-06-11 18:27:39 +0100

    version 1.0.0

--- system_monitor.sh
+++ system_monitor.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 
-echo "Hello World"
-touch created_using_vscode.txt
-mkdir new_dir
-mv created_using_vscode.txt new_dir
-cd new_dir
-echo "The test was succesful" > created_using_vscode.txt
-cat created_using_vscode.txt
-number=30
-echo $number
-echo {1..10}
-echo "What's your name?"
-read name
-
-echo "Hello, $name!. Nice to meet you"
+# Define the threshold values for CPU, memory, and disk usage
+CPU_THRESHOLD=80
+MEMORY_THRESHOLD=80
+DISK_THRESHOLD=80
